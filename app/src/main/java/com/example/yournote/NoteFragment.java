7022b839a2a854144f323fe33afd7c658ce3ceb7
kangert <kangerts9@gmail.com>
2021-08-11 22:24:50 +0300

    lesson6

--- app/src/main/java/com/example/yournote/NoteFragment.java
+++ app/src/main/java/com/example/yournote/NoteFragment.java
@@ -1,44 +1,73 @@
 package com.example.yournote;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class NoteFragment extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
+    //private boolean isLand;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-
-    public NoteFragment() {
-        // Required empty public constructor
-    }
-
-    public static NoteFragment newInstance(String param1, String param2) {
-        NoteFragment fragment = new NoteFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note, container, false);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initList(view);
+    }
+
+    private void initList(View view) {
+
+        LinearLayout linearLayout = (LinearLayout) view;
+
+        String[] notes = getResources().getStringArray(R.array.note_names);
+
+        for (int i = 0; i < notes.length; i++) {
+            TextView textView = new TextView(getContext());
+            textView.setText(notes[i]);
+            textView.setTextSize(30);
+            final int index = i;
+
+            textView.setOnClickListener(v -> {
+                showText(index);
+            });
+
+            linearLayout.addView(textView);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_note, container, false);
+    private void showText (int index) {
+//        if(isLand){
+//            showTextLand(index);
+//        } else {
+            showTextPort(index);
+      //  }
+
+    }
+
+//    private void showTextLand(int index) {
+        //TextFragment fragment = new TextFragment().newInstance(index);
+//    }
+
+    private void showTextPort (int index) {
+        Intent intent = new Intent();
+        intent.setClass(getActivity(), TextActivity.class);
+        intent.putExtra(TextFragment.ARG_INDEX, index);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/yournote/TextFragment.java
@@ -0,0 +1,44 @@
+package com.example.yournote;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class TextFragment extends Fragment {
+
+    static final String ARG_INDEX = "index";
+    private int index;
+
+    public static TextFragment newInstance(int index) {
+        TextFragment f = new TextFragment();
+
+        Bundle args = new Bundle();
+        args.putInt(ARG_INDEX, index);
+        f.setArguments(args);
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_INDEX);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_text, container, false);
+        TextView textView = view.findViewById(R.id.text);
+        String[] text = getResources().getStringArray(R.array.notes);
+        textView.setText(text[index-1]);
+        textView.setTextSize(45);
+        return view;
+    }
+}
