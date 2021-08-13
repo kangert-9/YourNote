edc173c9302c29f2fd4f0329c9565915b157ecc5
kangert <kangerts9@gmail.com>
2021-08-13 08:10:16 +0300

    lesson7

--- app/src/main/java/com/example/yournote/MainActivity.java
+++ app/src/main/java/com/example/yournote/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.yournote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
